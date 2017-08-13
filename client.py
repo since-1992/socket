52e991b0ddb04ccc1c4f6d0aa75ffe67f3438ed0
since-1992 <2849222658@qq.com>
2017-08-13 19:41:02 +0800

    add comment for client.py

--- client.py
+++ client.py
@@ -2,12 +2,15 @@
 import socket
 sk=socket.socket()
 ip_port=("127.0.0.1",9999)
+#the client connects,use connect(ip_port)
 sk.connect(ip_port)
 while True:
 	print "Computer:"
 	str=raw_input()
+	#the client sends message to the server_reply
 	sk.sendall(str)
 	server_reply=sk.recv(1024)
 	print "Liuyunfei:"
 	print server_reply
+        #do not forget to close the socket
 	sk.close()
